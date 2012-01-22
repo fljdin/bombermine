3113185c330286dd7e51cf67667c6312525d2dcf
Florent Jardin <florent.jardin@gmail.com>
2012-01-22 20:47:06 +0100

    Conflict correction

--- src/com/github/myorama/bombermine/Bombermine.java
+++ src/com/github/myorama/bombermine/Bombermine.java
@@ -3,14 +3,12 @@ package com.github.myorama.bombermine;
 import com.github.myorama.bombermine.commandexecutors.BombermineCommandExecutor;
 import com.github.myorama.bombermine.listeners.BomberminePlayerListener;
 import com.github.myorama.bombermine.models.CTFGame;
-<<<<<<< HEAD
-import java.util.ArrayList;
-import java.util.List;
-=======
 import com.github.myorama.bombermine.models.Traps;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import java.io.File;
->>>>>>> Landmines module
 import java.util.logging.Logger;
 
 import org.bukkit.Location;
@@ -26,11 +24,8 @@ public class Bombermine extends JavaPlugin {
 
 	public static final Logger log = Logger.getLogger(Bombermine.class.getName());
 	private CTFGame ctfGame = null;
-<<<<<<< HEAD
 	private CommandExecutor cmdExec = null;
-=======
 	private Traps traps = null;
->>>>>>> Landmines module
 	
 	@Override
 	public void onEnable() {
@@ -48,18 +43,12 @@ public class Bombermine extends JavaPlugin {
 		this.ctfGame = new CTFGame(this);
 		this.ctfGame.initialize();
 		
-<<<<<<< HEAD
 		// Instanting CommandExecutor
 		this.cmdExec = new BombermineCommandExecutor(this);
 		this.getCommand("bm").setExecutor(cmdExec);
-=======
+
 		// Initialize Traps feature
 		this.traps = new Traps(new File("./plugins/Bombermine/traps.coord"));
-		this.traps.addTrap(new Location(this.getServer().getWorlds().get(0), -1, -1, -1));
-		this.traps.addTrap(new Location(this.getServer().getWorlds().get(0), -1, -1, -1));
-		this.traps.addTrap(new Location(this.getServer().getWorlds().get(0), -1, -2, -1));
-		//this.traps.removeTrap(new Location(this.getServer().getWorlds().get(0), -1, -1, -1));
->>>>>>> Landmines module
 		
 		log.info("Bombermine plugin has been enabled.");
 	}
