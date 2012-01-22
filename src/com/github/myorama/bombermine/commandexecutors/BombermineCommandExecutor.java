c92154f9afe1dbf29ab01c26462d4f188bfe11d2
Nittero <Nittero@192.168.1.170>
2012-01-22 21:44:30 +0100

    Command executor /bm command

--- src/com/github/myorama/bombermine/commandexecutors/BombermineCommandExecutor.java
+++ src/com/github/myorama/bombermine/commandexecutors/BombermineCommandExecutor.java
@@ -5,6 +5,7 @@
 package com.github.myorama.bombermine.commandexecutors;
 
 import com.github.myorama.bombermine.Bombermine;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -14,17 +15,22 @@ import org.bukkit.command.CommandSender;
  * @author Nittero
  */
 public class BombermineCommandExecutor implements CommandExecutor {
+
 	private Bombermine plugin;
-	
+
 	public BombermineCommandExecutor(Bombermine instance) {
 		this.plugin = instance;
 	}
-	
+
 	@Override
-	public boolean onCommand(CommandSender cs, Command cmnd, String string, String[] strings) {
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		// TODO show help for command and manage sub commands
 		// TODO register permissions for each command
+		// TODO test if sender is console or player
+		if(args.length < 2)
+		{
+			sender.sendMessage(ChatColor.GREEN + "/bm join <team_id>");
+		}
 		return false;
 	}
-	
 }
