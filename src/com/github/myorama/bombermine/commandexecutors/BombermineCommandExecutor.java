be9d260d477378c08d73e2af697e8aed86224154
Nittero <Nittero@192.168.1.170>
2012-01-25 14:50:44 +0100

    Leave team commands implemented - /bm leave - /bm leave <player>

--- src/com/github/myorama/bombermine/commandexecutors/BombermineCommandExecutor.java
+++ src/com/github/myorama/bombermine/commandexecutors/BombermineCommandExecutor.java
@@ -63,7 +63,7 @@ public class BombermineCommandExecutor implements CommandExecutor {
 			}
 		} else if (args.length > 0) {
 			if (args[0].equals("join")) {
-				if(args.length == 1){
+				if(args.length == 1){ // join
 					if(player != null){
 						if(hasPlayerRights(player)){
 							if(!this.plugin.getCtfGame().setPlayerTeam(player)){
@@ -78,7 +78,7 @@ public class BombermineCommandExecutor implements CommandExecutor {
 						sender.sendMessage(ERROR_PLAYER_ONLY);
 					}
 				}
-				else if (args.length == 2) {
+				else if (args.length == 2) { // join <team>
 					if(player != null){
 						if(hasPlayerRights(player)){
 							Team team = plugin.getCtfGame().getTeamById(args[1]);
@@ -97,21 +97,20 @@ public class BombermineCommandExecutor implements CommandExecutor {
 					else{
 						sender.sendMessage(ERROR_PLAYER_ONLY);
 					}
-				} else if (args.length == 3) {
+				} else if (args.length == 3) { // join <player> <team>
 					if(hasModRights(player)){
 						Team team = plugin.getCtfGame().getTeamById(args[2]);
-						if(team == null){
-							sender.sendMessage(errColor + "Team id \"" + args[2] + "\" does not exist");
-						} else {
+						if(team != null){
 							Player tPlayer = Bukkit.getServer().getPlayer(args[1]);
-							if(tPlayer == null){
-								sender.sendMessage(errColor + "Player " + args[1] + " is not online");
-							}
-							else{
-								if(this.plugin.getCtfGame().setPlayerTeam(tPlayer, team)){
+							if(tPlayer != null){
+								if(!this.plugin.getCtfGame().setPlayerTeam(tPlayer, team)){
 									sender.sendMessage(errColor + "Team is full");
 								}
+							}else{
+								sender.sendMessage(errColor + "Player " + args[1] + " is not online");
 							}
+						}else{
+							sender.sendMessage(errColor + "Team id \"" + args[2] + "\" does not exist");
 						}
 					}
 					else{
@@ -126,6 +125,26 @@ public class BombermineCommandExecutor implements CommandExecutor {
 						}
 					} else {
 						sender.sendMessage(msgColor + "/bm join <player> <team>");
+					}
+				}
+			}
+			else if (args[0].equals("leave")) {
+				if(args.length == 1){ // leave
+					if(player != null){
+						if(hasPlayerRights(player)){
+							this.plugin.getCtfGame().removePlayer(player);
+						}
+					}else{
+						sender.sendMessage(errColor + ERROR_PLAYER_ONLY);
+					}
+				}else if(args.length == 2){ // leave <player>
+					Player tPlayer = Bukkit.getServer().getPlayer(args[1]);
+					if(tPlayer != null){
+						if(hasModRights(player)){
+							this.plugin.getCtfGame().removePlayer(player);
+						}
+					}else{
+						sender.sendMessage(errColor + "Player " + args[1] + " is not online");
 					}
 				}
 			}
