70ce859e8bcc3300aba5d11438153a50a56edd3a
Florent Jardin <florent.jardin@gmail.com>
2012-01-30 08:00:44 +0100

    Teleport item

--- src/com/github/myorama/bombermine/listeners/ItemListener.java
+++ src/com/github/myorama/bombermine/listeners/ItemListener.java
@@ -1,9 +1,14 @@
 package com.github.myorama.bombermine.listeners;
 
 import com.github.myorama.bombermine.Bombermine;
+import com.github.myorama.bombermine.models.Team;
+
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerEggThrowEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 /**
  * @author myorama
@@ -22,5 +27,16 @@ public class ItemListener implements Listener {
 	@EventHandler
 	public void throwGrenade(PlayerEggThrowEvent event) {
 		// TODO catch impact location et perform explosion
+	}
+	
+	@EventHandler
+	public void useTeleportItem(PlayerInteractEvent event) {
+		Player player = event.getPlayer();
+		Team team = plugin.getCtfGame().getPlayerTeam(player);
+		
+		if (event.getAction() == Action.RIGHT_CLICK_AIR
+				&& event.getMaterial().name().contains("RECORD")) {
+			player.teleport(team.getSpawnLoc());
+		}
 	}
 }
